082951c99060cadb35720a8b436d4cb277383a92
Sushil Hemrom <sushil500530@gmail.com>
2024-01-16 23:07:27 +0600

    add my task and task card pages

---
+++ src/components/tasks/TaskCard.jsx
@@ -0,0 +1,59 @@
+import { ArrowRightIcon, TrashIcon } from "@heroicons/react/24/solid";
+
+const TaskCard = () => {
+    // const dispatch = useDispatch();
+    // console.log(task);
+    // let updatedStatus;
+    // if(task.status === 'pending'){
+    //   updatedStatus = 'running';
+    // }
+    // else if(task.status === 'running'){
+    //    updatedStatus = 'done';
+    // } 
+    // else{
+    //   updatedStatus= 'archived';
+    // }
+    const task = {
+      id: 1,
+      status: 'pending',
+      title: 'Remove Button',
+      description:
+        'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+      date: '2023-08-28',
+      assignedTo: 'Mir Hussain',
+      priority: 'high',
+    };
+  
+  
+    return (
+        <div className="bg-secondary/10 rounded-md p-5">
+        <h1
+          className={`text-lg font-semibold mb-3  ${
+            task?.priority === 'hard' ? 'text-red-500' : ''
+          } ${task?.priority === 'medium' ? 'text-yellow-500' : ''} ${
+            task?.priority === 'easy' ? 'text-green-500' : ''
+          }`}
+        >
+          {task?.title}
+        </h1>
+        <p className="mb-3">{task?.description}</p>
+        <p className="text-sm">Assigned to - {task?.name}</p>
+        <div className="flex justify-between mt-3">
+          <p>{task?.dateline}</p>
+          {/* onClick={()=> dispatch(removeTask(task?.id))} */}
+          <div className="flex gap-3">
+            <button title="Delete">
+              <TrashIcon className="h-5 w-5 text-red-500" />
+            </button>
+            {/* onClick={()=> dispatch(updateTask({id:task?.id, status: updatedStatus}))} */}
+            <button  title="Update Status"
+            >
+              <ArrowRightIcon className="h-5 w-5 text-primary" />
+            </button>
+          </div>
+        </div>
+      </div>  
+    );
+};
+
+export default TaskCard;
